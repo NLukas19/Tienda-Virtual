319f3222da1bf939401ffe4cc32c37358989821a
NLukas19 <lukasnahuel241@gmail.com>
2024-05-30 20:03:13 -0300

    Visuales de Productos y mejores en el Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductosComponent } from './productos/productos.component';
 import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './cards/cards.component';
 
 export const routes: Routes = [
     { path:"", component: HomeComponent},
     { path: 'productos', component: ProductosComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'cards', component: CardsComponent },
     {path: '**', redirectTo: "home"},
 ];
